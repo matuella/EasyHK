373a190a0cde3a2f13f76c26ae08fa7cc7bbe9f1
Guilherme Carlos Matuella <guilherme1matuella@gmail.com>
2018-04-29 18:04:52 -0300

    Fixing Podspec filestructure + deployment target

---
+++ EasyHK/Classes/Extensions/HKDocumentTypeIdentifier+EasyHK.swift
@@ -0,0 +1,23 @@
+//
+//  HKDocumentTypeIdentifier+EasyHK.swift
+//  EasyHK
+//
+//  Created by Guilherme Carlos Matuella on 29/04/18.
+//  Copyright © 2018 gmatuella. All rights reserved.
+//
+
+import HealthKit
+
+// TODO: adapt the HKDocumentTypeIdentifier to be available when its required version is met.
+//@available(iOS 10.0, *)
+//extension HKDocumentTypeIdentifier: EasyHKReadWrite {
+//
+//    public var associatedObjectType: HKObjectType? {
+//        return HKObjectType.documentType(forIdentifier: self)
+//    }
+//
+//    public var associatedSampleType: HKSampleType? {
+//        return HKSampleType.documentType(forIdentifier: self)
+//    }
+//}
+
